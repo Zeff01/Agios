12a1cbc8d7cff206434f81aaed0b0389f6779884
zeff <jzeffsomera@gmail.com>
2024-04-23 15:07:48 +0800

    delete comments

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -4,28 +4,21 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: NextApiRequest) {
   try {
-    // Parse the request body
     const { email, password, firstname, lastname } = req.body;
     console.log("req.body:", req.body);
-
-    // Create a Supabase client instance
     const supabase = createClient();
 
-    // Check if email and password are provided
     if (!email || !password) {
       return NextResponse.json(
         { error: "Email and password are required" },
         { status: 400 }
       );
     }
-
-    // Sign up the user using Supabase
     const { data, error } = await supabase.auth.signUp({
       email: email,
       password: password,
     });
 
-    // Handle errors during sign-up
     if (error) {
       console.error("Signup error:", error);
       return NextResponse.json(
@@ -34,20 +27,15 @@ export async function POST(req: NextApiRequest) {
       );
     }
 
-    // Extract the user object from the response data
     const { user } = data;
 
-    // Check if the user object exists
     if (user) {
-      // Prepare user profile data
       const profileData = { id: user.id, firstname, lastname, isPaid: false };
 
-      // Insert user profile data into the database
       const { data: profile, error: profileError } = await supabase
         .from("profiles")
         .insert([profileData]);
 
-      // Handle errors during profile creation
       if (profileError) {
         console.error("Profile creation error:", profileError);
         return NextResponse.json(
@@ -56,7 +44,6 @@ export async function POST(req: NextApiRequest) {
         );
       }
 
-      // Return success response
       return NextResponse.json(
         {
           message: "Check your email to continue the sign-in process",
@@ -65,14 +52,12 @@ export async function POST(req: NextApiRequest) {
         { status: 200 }
       );
     } else {
-      // Return error response if user object is not found
       return NextResponse.json(
         { error: "User was not created successfully" },
         { status: 500 }
       );
     }
   } catch (err) {
-    // Handle unexpected errors
     console.error("Unexpected error:", err);
     return NextResponse.json(
       { error: "Internal server error" },
